47554913d9698e4e2c592bff241fa725a6c1eabc
wernerfragner <iwerkG8378lsfki>
2022-11-05 14:45:31 +0100

    #10 added reflect-config.json with dynamically instantiated classes

--- collector-app/src/main/java/org/oekofen/collector/CollectorApplication.java
+++ collector-app/src/main/java/org/oekofen/collector/CollectorApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@TypeHint(types = {ObjectTransformations.class, ObjectTransformation.class, FieldTransformation.class})
 @SpringBootApplication
 @EnableScheduling
 public class CollectorApplication
